608abc8a6b565a659b32c1cb7797bb46685a98f1
theo <80356772+pinklittleman@users.noreply.github.com>
2022-11-08 13:43:42 +0000

    percent

---
+++ index.js
@@ -0,0 +1,33 @@
+const socket = io.connect('wss://pinky.uk.to:5000');
+
+var email
+
+function send(){
+    console.log('pressed')
+    email = document.getElementById('textin').value;
+    socket.emit('test', email)
+}
+    
+    socket.on('chatlogs',toast)
+
+    function toast(data){
+        document.getElementById('chat').innerHTML = data
+    }
+
+const updateScrollPercentage = function() { 
+const heightOfWindow = window.innerHeight,
+	contentScrolled = window.pageYOffset,
+	bodyHeight = document.body.offsetHeight
+ 
+    if(bodyHeight - contentScrolled <= heightOfWindow) {
+    		console.log('100%')
+    }
+    else {
+    		const total = bodyHeight - heightOfWindow,
+    			got = contentScrolled,
+    			percent = parseInt((got/total) * 100)
+    		console.log(percent+'%')
+    }
+}
+ 
+window.addEventListener('scroll', updateScrollPercentage)
