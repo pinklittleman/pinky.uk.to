cdfd3df7c6b887d88f55b0765d43154e1e0665d2
theo <80356772+pinklittleman@users.noreply.github.com>
2022-11-10 22:48:23 +0000

    Update upload.php

--- files/upload.php
+++ files/upload.php
@@ -42,7 +42,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded. <a href='https://pinky.uk.to/files/uploads/'>go to uploads</a>";
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
