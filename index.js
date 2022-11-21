f8491d52d60942a484caa28e288c03c99cc2234c
theo <80356772+pinklittleman@users.noreply.github.com>
2022-11-21 18:11:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,6 @@ function draw() {
   ctx.rotate((Math.PI/180) * state.rotation)
 
   ctx.strokeStyle = 'white'
-  ctx.fillStyle = 'white'
   ctx.lineWidth = 2
   ctx.beginPath()
   ctx.moveTo(0, 0)
@@ -114,7 +113,6 @@ function draw() {
   ctx.lineTo(0, -20)
   ctx.lineTo(-10, 10)
   ctx.lineTo(0, 0)
-    ctx.fillRect(-50, -50, 100, 100);
   ctx.closePath()
   ctx.stroke()
   ctx.restore()
