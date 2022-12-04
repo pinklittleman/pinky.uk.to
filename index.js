30c7c5592f42488883f143fc66219966f4ebe422
theo <80356772+pinklittleman@users.noreply.github.com>
2022-12-04 02:17:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,11 @@ function checkupdates(){
     if(car.UP === false){
         if(car.momentum_y < 0){
             car.momentum_y += 0.005
-            console.log(car.momentum_y.toFixed(2))
+            if(car.momentum_y.toFixed(2) === 0.00){
+                setTimeout(() => {
+                    car.momentum_y = 0
+                }, 500);
+            }
             
         }
     }
