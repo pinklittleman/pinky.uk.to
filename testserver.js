f8ba2f4b3d58be464b2846ad8019b060370572b8
theo <80356772+pinklittleman@users.noreply.github.com>
2022-12-18 02:47:21 +0000

    Automatic commit: from the sneeky python script

---
+++ testserver.js
@@ -0,0 +1,10 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 5000 });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    ws.send(`Echo: ${message}`);
+  });
+});
