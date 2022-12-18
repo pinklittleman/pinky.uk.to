9f7df85a9a1fe45d09664705d4111f29b9e43010
theo <80356772+pinklittleman@users.noreply.github.com>
2022-12-18 02:55:13 +0000

    Automatic commit: from the sneeky python script

--- testserver.js
+++ testserver.js
@@ -11,7 +11,11 @@ const wss = new WebSocket.Server({ server });
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log(`Received message: ${message}`);
-    
+    wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message);
+        }
+      });
   });
 });
 
