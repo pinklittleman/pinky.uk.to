587eb1fd68ba76525dc826f513fc9d5e31471bba
pinklittleman <theominer3000@gmail.com>
2022-12-25 13:08:50 +0000

    Automatic commit: from the sneeky python script

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d")
 let x = 100, y = 100
 canvas.width = innerWidth
 canvas.height = innerHeight
-inc
+let inc, dec
 
 function draw(){
    ctx.fillStyle = "#9916ff";
